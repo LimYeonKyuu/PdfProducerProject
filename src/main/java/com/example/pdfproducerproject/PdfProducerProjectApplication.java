76351b277dd9b85b5592a8dc332b76fd0ed77c20
lim-yeonkyu <kevin000606@handong.ac.kr>
2023-08-17 17:11:19 +0900

    Project setting

---
+++ src/main/java/com/example/pdfproducerproject/PdfProducerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdfproducerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfProducerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfProducerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pdfproducerproject/PdfProducerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdfproducerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfProducerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
