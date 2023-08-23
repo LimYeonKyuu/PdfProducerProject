8b3e284c2f196093a3b3c1ee9134228f98b73dc6
lim-yeonkyu <kevin000606@handong.ac.kr>
2023-08-23 16:03:15 +0900

    perf: Change a PDF file to include a background image
    Change a PDF file to include a background image.

--- src/main/java/com/example/pdfproducerproject/view/PdfDownView.java
+++ src/main/java/com/example/pdfproducerproject/view/PdfDownView.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import com.example.pdfproducerproject.vo.CampVO;
 import com.example.pdfproducerproject.vo.StudentVO;
 import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfContentByte;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
@@ -28,6 +29,23 @@ public class PdfDownView extends AbstractPdfView {
             HttpServletRequest request,
             HttpServletResponse response) throws Exception {
 
+        // 여백을 조절하기 위해 PdfContentByte 객체를 가져옴
+        PdfContentByte contentByte = writer.getDirectContent();
+
+        // 배경 이미지 파일 로드
+        Image backgroundImage = Image.getInstance(PdfDownView.class.getResource("/img/background.png"));
+
+        // 배경 이미지를 페이지에 추가
+        backgroundImage.setAbsolutePosition(0, 0);
+        backgroundImage.scaleToFit(doc.getPageSize().getWidth(), doc.getPageSize().getHeight());
+
+        // 이미지의 투명도 설정
+        backgroundImage.setTransparency(new int[]{255, 255});
+
+        // 이미지를 컨텐츠에 추가
+        contentByte.addImage(backgroundImage);
+
+
         List<StudentVO> studentList = (List<StudentVO>) model.get("list");
         CampVO campVO = (CampVO) model.get("c");
         // 기본 폰트 설정 - 폰트에 따라 한글 출력 여부가 결정된다.
@@ -36,6 +54,7 @@ public class PdfDownView extends AbstractPdfView {
         Font bigFont = new Font(bfKorea, 45);
         Font midFont = new Font(bfKorea, 25);
 
+        doc.add(new Paragraph("\n\n"));
         // 수료증 제목 추가
         Paragraph subject = new Paragraph("수료증", bigFont);
         subject.setAlignment(Paragraph.ALIGN_CENTER);
@@ -45,31 +64,31 @@ public class PdfDownView extends AbstractPdfView {
 
         //이름 추가
         Paragraph name = new Paragraph("성명: " + studentList.get(0).getName(), font);
-        name.setIndentationLeft(370);
+        name.setIndentationLeft(350);
         doc.add(name);
 
         //학번 추가
         Paragraph studentId = new Paragraph("학번: " + studentList.get(0).getStudentId(), font);
-        studentId.setIndentationLeft(370);
+        studentId.setIndentationLeft(350);
         doc.add(studentId);
 
         //학부 추가
         Paragraph department = new Paragraph("학부: " + studentList.get(0).getDepartment(), font);
-        department.setIndentationLeft(370);
+        department.setIndentationLeft(350);
         doc.add(department);
 
         doc.add(new Paragraph("\n"));
         //캠프명 추가
         Paragraph camp = new Paragraph("캠프명: " + campVO.getName(), font);
-        camp.setIndentationLeft(40);
+        camp.setIndentationLeft(30);
         doc.add(camp);
 
         //기간 추가
         Paragraph date = new Paragraph("기간: " + campVO.getFormatEndDate() + "~"+ campVO.getFormatEndDate(), font);
-        date.setIndentationLeft(40);
+        date.setIndentationLeft(30);
         doc.add(date);
 
-        doc.add(new Paragraph("\n\n\n\n\n"));
+        doc.add(new Paragraph("\n\n\n\n"));
 
         //내용 추가
         Paragraph content = new Paragraph("위 사람은 "+campVO.getFormatStartDate()+ "부터 "+ campVO.getFormatEndDate()+"까지 진행되는 "+ campVO.getName() + " 과정을 성실히 이수하였음으로 이 증서를 드립니다.", midFont);
@@ -79,7 +98,7 @@ public class PdfDownView extends AbstractPdfView {
         doc.add(content);
 
 
-        doc.add(new Paragraph("\n\n\n\n\n"));
+        doc.add(new Paragraph("\n\n\n\n\n\n"));
 
         //오늘 날짜 추가
         LocalDate now = LocalDate.now();
@@ -88,9 +107,5 @@ public class PdfDownView extends AbstractPdfView {
         Paragraph nowDate = new Paragraph(formattedDate ,font);
         nowDate.setAlignment(Paragraph.ALIGN_CENTER);
         doc.add(nowDate);
-
-        doc.newPage();
-        doc.add(subject);
-
     }
 }
